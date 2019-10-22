a5f864935838b7d9abb16e6c17e2d91270162230
phi <phi1618jp@gmail.com>
2019-10-22 14:54:18 +0900

    setup page

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 const yaml = require('js-yaml');
 const config = yaml.safeLoad(fs.readFileSync(`${process.cwd()}/dotou.yaml`));
 const express = require('express');
 const app = express();
 
+// setup express
+app.use(express.static('public'));
+
+// setup pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// setup routing
 app.get('/', async (req, res) => {
+  res.render('index');
+});
+
+app.get('/config', async (req, res) => {
   res.json(config);
 });
 
