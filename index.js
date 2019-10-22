ab65d69774e38df3c720382d1fb0e812faf6afd8
phi <phi1618jp@gmail.com>
2019-10-22 14:31:36 +0900

    setup express

--- index.js
+++ index.js
@@ -3,3 +3,17 @@
 process.env["NODE_CONFIG_DIR"] = process.cwd();
 const config = require('config');
 console.log(config);
+
+const express = require('express');
+const app = express();
+
+app.get('/', async (req, res) => {
+  res.json(config);
+});
+
+// Start the server
+const PORT = process.env.PORT || 8887;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
